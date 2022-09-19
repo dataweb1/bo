d8720062d37a7a6851c31093a8bbf6fbf94097e5
dataweb <bert@data-web.be>
2022-09-20 00:17:42 +0200

    GGL map support.

---
+++ modules/bo_ggl_map/js/bo_ggl_map.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Handle the bo_ggl_map specific JS.
+ */
+
+(function ($, Drupal, drupalSettings) {
+  //'use strict';
+
+  /**
+   * Drupal behavior.
+   */
+  Drupal.behaviors.bo_ggl_map = {
+    attach: function (context, drupalSettings) {
+    }
+  };
+
+  $(window).on({
+    'bo:refreshView': function boGglMapRefreshView(event, result) {
+      console.log(result.view_dom_id);
+      if ($(".js-view-dom-id-" + result.view_dom_id).find(' #ggl_map').length > 0) {
+        Drupal.gglMap.attached = false;
+        //}
+      }
+    }
+  });
+
+})(jQuery, Drupal, drupalSettings);
