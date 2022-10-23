1095b429083fd0fc4b7bfed47713b2c6c2b4af65
dataweb <bert@data-web.be>
2022-10-23 14:23:09 +0200

    Error fix on empty html content.

--- src/Service/BoVarsHelper.php
+++ src/Service/BoVarsHelper.php
@@ -37,20 +37,21 @@ class BoVarsHelper {
   public function replaceInlineImagesByImageStyle(&$content, $imageStyle) {
     $doc=new \DOMDocument();
     $doc->loadHTML($content);
-    $xml=\simplexml_import_dom($doc); // just to make xpath more simple
-    $images=$xml->xpath('//img');
-    $images_transformed = FALSE;
-    foreach ($images as $img) {
-      $public_files_folder = $this->fileUrlGenerator->generateString('public://');
-      $image_uri = str_replace($public_files_folder . 'inline-images/', 'public://inline-images/', urldecode($img['src']));
-      $style = ImageStyle::load($imageStyle);
-      if ($image_url = $style->buildUrl($image_uri)) {
-        $content = str_replace($img['src'], $image_url, $content);
-        $images_transformed = TRUE;
+    if ($xml=\simplexml_import_dom($doc)) { // just to make xpath more simple
+      $images = $xml->xpath('//img');
+      $images_transformed = FALSE;
+      foreach ($images as $img) {
+        $public_files_folder = $this->fileUrlGenerator->generateString('public://');
+        $image_uri = str_replace($public_files_folder . 'inline-images/', 'public://inline-images/', urldecode($img['src']));
+        $style = ImageStyle::load($imageStyle);
+        if ($image_url = $style->buildUrl($image_uri)) {
+          $content = str_replace($img['src'], $image_url, $content);
+          $images_transformed = TRUE;
+        }
       }
-    }
-    if ($images_transformed) {
-      $content = Markup::create($content);
+      if ($images_transformed) {
+        $content = Markup::create($content);
+      }
     }
   }
 
